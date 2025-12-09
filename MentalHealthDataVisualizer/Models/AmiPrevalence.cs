f44260d6bf4d00040228cb7ea4958261d1d4ecec
Seth Kietz <skietz01@gmail.com>
2025-12-08 21:11:50 -0500

    Add project files.

---
+++ MentalHealthDataVisualizer/Models/AmiPrevalence.cs
@@ -0,0 +1,10 @@
+﻿namespace MentalHealthDataVisualizer.Models
+{
+    public class AmiPrevalence
+    {
+        public int data_year { get; set; }
+        public string age_category { get; set; }
+        public string demographic {  get; set; }
+        public int tally {  get; set; }
+    }
+}
---
+++ MentalHealthDataVisualizer/Program.cs
@@ -0,0 +1,36 @@
+using MentalHealthDataVisualizer;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Retrieve the connection string from appsettings.json
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+// Add DbContext to the DI container
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
