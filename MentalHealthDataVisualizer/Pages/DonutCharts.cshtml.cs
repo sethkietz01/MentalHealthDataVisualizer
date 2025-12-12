591d36a91f9e2cdc3e99308de4e65fbfd8792cc1
Seth Kietz <skietz01@gmail.com>
2025-12-12 17:52:39 -0500

    Update Index.cshtml headers and update Table View screenshot

---
+++ MentalHealthDataVisualizer/Pages/DonutCharts.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MentalHealthDataVisualizer.Pages
+{
+    public class DonutChartsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
