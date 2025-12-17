d661c9a80e808efd46352417f0626a33ec866808
Seth Kietz <skietz01@gmail.com>
2025-12-17 11:55:14 -0500

    Add Scatter Charts and update screenshots

---
+++ MentalHealthDataVisualizer/Pages/ScatterCharts.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MentalHealthDataVisualizer.Pages
+{
+    public class ScatterChartsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
